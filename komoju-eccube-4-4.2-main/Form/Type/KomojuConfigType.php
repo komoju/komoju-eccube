a67f849002d37022b1598ac9a637deb8b5e7adad
Aaron Clark <aclark@degica.com>
2026-04-07 13:44:24 +0900

    update configtype

--- komoju-eccube-4-4.2-main/Form/Type/KomojuConfigType.php
+++ komoju-eccube-4-4.2-main/Form/Type/KomojuConfigType.php
@@ -12,51 +12,49 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 
 class KomojuConfigType extends AbstractType{
-    
+
+    private function isCustomApiUrl(): bool
+    {
+        $url = getenv('KOMOJU_API_URL');
+        return $url && $url !== 'https://komoju.com';
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $option){
+        $keyConstraints = function ($emptyMessage, $regexMessage) {
+            $constraints = [
+                new Assert\NotBlank(array('message' => trans($emptyMessage))),
+            ];
+            if (!$this->isCustomApiUrl()) {
+                $constraints[] = new Assert\Regex(array(
+                    'pattern' => '/^\w+$/',
+                    'match' => true,
+                    'message' => trans($regexMessage),
+                ));
+            }
+            return $constraints;
+        };
+
         $builder
             ->add('publishable_key', TextType::class, [
                 'required'  =>  true,
-                'constraints' => [
-                    new Assert\NotBlank(array(
-                        'message' => trans('komoju_multipay.admin.config.error.publishable_key.empty')
-                        )
-                    ),
-                    new Assert\Regex(array(
-                            'pattern' => '/^\w+$/',
-                            'match' => true,
-                            'message' => trans('komoju_multipay.admin.config.error.publishable_key.regex_invalid')
-                        )
-                    )
-                ]
+                'constraints' => $keyConstraints(
+                    'komoju_multipay.admin.config.error.publishable_key.empty',
+                    'komoju_multipay.admin.config.error.publishable_key.regex_invalid'
+                )
             ])
             ->add('secret_key', TextType::class, [
                 'required'      =>  true,
-                'constraints'   =>  [
-                    new Assert\NotBlank(array(
-                        'message'   =>  trans('komoju_multipay.admin.config.error.secret_key.empty')
-                    )),
-                    new Assert\Regex(array(
-                            'pattern' => '/^\w+$/',
-                            'match' => true,
-                            'message' => trans('komoju_multipay.admin.config.error.secret_key.regex_invalid')
-                        )
-                    )
-                ]
+                'constraints'   =>  $keyConstraints(
+                    'komoju_multipay.admin.config.error.secret_key.empty',
+                    'komoju_multipay.admin.config.error.secret_key.regex_invalid'
+                )
             ])
             ->add('merchant_uuid', TextType::class, [
                 'required'      =>  true,
-                'constraints'    =>  [
-                    new Assert\NotBlank(array(
-                        'message'   =>  trans('komoju_multipay.admin.config.error.merchant_uuid.empty')
-                    )),
-                    new Assert\Regex(array(
-                            'pattern' => '/^\w+$/',
-                            'match' => true,
-                            'message' => trans('komoju_multipay.admin.config.error.merchant_uuid.regex_invalid')
-                        )
-                    )
-                ]
+                'constraints'    =>  $keyConstraints(
+                    'komoju_multipay.admin.config.error.merchant_uuid.empty',
+                    'komoju_multipay.admin.config.error.merchant_uuid.regex_invalid'
+                )
             ])
             ->add('capture_on', ChoiceType::class, [
                 'required'  =>  true,
